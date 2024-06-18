f0749a24425f4841a856124ab230edd38d9fa8bd
shravani_jorrigala <shravanijorrigala821@gmail.com>
2024-06-18 17:24:55 +0530

    greater

--- kmmt01esd22/C_Basics/conditional_statements/3_3bigger.c
+++ kmmt01esd22/C_Basics/conditional_statements/3_3bigger.c
@@ -7,7 +7,7 @@ int main()
 	int a,b,c;
 	printf("enter a,b & c values\n");
 	scanf("%d%d%d",&a,&b,&c);
-	if(a>b)
+/*	if(a>b)
 	{
 		printf("%d is greater\n",a);
 	}
@@ -18,5 +18,16 @@ int main()
 	else if(c>a)
 	{
 		printf("%d is greater\n",c);
+	}*/
+	
+	if(a>b && a>c)
+	{
+	printf("%d is greater\n",a);
 	}
+	else if(b>c)
+	{
+	printf("%d is greater\n",b);
+	}
+	else
+		printf("%d is greater\n",c);
 }
--- kmmt01esd22/C_Basics/conditional_statements/7_4bigger.c
+++ kmmt01esd22/C_Basics/conditional_statements/7_4bigger.c
@@ -7,7 +7,7 @@ int main()
 	int a,b,c,d;
 	printf("enter a,b,c,d values\n");
 	scanf("%d%d%d%d",&a,&b,&c,&d);
-	if(a>b)
+/*	if(a>b)
 	{
 		if(a>c)
 			if(a>d)
@@ -23,5 +23,20 @@ int main()
 				printf("%d is bigger\n",c);
 	}
 	else
-		printf("%d is bigger\n",d);
+		printf("%d is bigger\n",d);*/
+
+	if(a>b && a>c && a>d)
+	{
+	printf("%d is greater\n",a);
+	}
+	else if(b>c && b>d)
+	{
+	printf("%d is greater\n",b);
+	}
+	else if(c>d)
+	{
+	printf("%d is greater\n",c);
+	}
+	else
+	printf("%d is greater\n",d);
 }
