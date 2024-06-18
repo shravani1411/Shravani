379604c0ee5efc97006066b16b99892b99ea969c
shravani_jorrigala <shravanijorrigala821@gmail.com>
2024-06-18 17:05:47 +0530

    greater_of_4

--- kmmt01esd22/C_Basics/conditional_statements/7_4bigger.c
+++ kmmt01esd22/C_Basics/conditional_statements/7_4bigger.c
@@ -1,22 +1,26 @@
 //7)Read 4 numbers and print which of them is biggest.
 
+
 #include<stdio.h>
 int main()
 {
 	int a,b,c,d;
 	printf("enter a,b,c,d values\n");
 	scanf("%d%d%d%d",&a,&b,&c,&d);
-	if(a>b && a>c && a>d)
+	if(a>b)
 	{
-		printf("%d is bigger\n",a);
+		if(a>c)
+			if(a>d)
+				printf("%d is bigger\n",a);
 	}
-	else if(b>c && b>d)
+	else if(b>c)
 	{
-		printf("%d is bigger\n",b);
+			if(b>d)
+				printf("%d is bigger\n",b);
 	}
 	else if(c>d)
 	{
-		printf("%d is bigger\n",c);
+				printf("%d is bigger\n",c);
 	}
 	else
 		printf("%d is bigger\n",d);
